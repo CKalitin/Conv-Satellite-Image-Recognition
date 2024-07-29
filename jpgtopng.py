cefa9e5d421dc866c9f7fa3a8343899a1cba1049
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2024-07-29 15:55:30 -0700

    Back to jpg
    The problem with the original dataset was clouds used cmyk colours instead of rgb. Converted to png but was ~100MB, now back to jpg in rgb

--- jpgtopng.py
+++ jpgtopng.py
@@ -2,8 +2,8 @@ import sys
 import os
 from PIL import Image
 
-source_folder = "C:\_Dev\Conv-Satellite-Image-Recognition\data\\water\\"
-destination_folder = "C:\_Dev\Conv-Satellite-Image-Recognition\datapng\\water\\"
+source_folder = "C:\_Dev\Conv-Satellite-Image-Recognition\datapng\\water\\"
+destination_folder = "C:\_Dev\Conv-Satellite-Image-Recognition\datajpg\\water\\"
 
 if not os.path.exists(destination_folder):
     # destination_folder does not exist
@@ -13,5 +13,5 @@ for imagefile in os.listdir(source_folder):
     img = Image.open(f'{source_folder}{imagefile}')
     #img = img.convert('RGB')
     clean_imgname = os.path.splitext(imagefile)[0]
-    img.save(f'{destination_folder}{clean_imgname}.png', 'png')
+    img.save(f'{destination_folder}{clean_imgname}.jpg', 'jpeg')
     print('Conversion done!')
